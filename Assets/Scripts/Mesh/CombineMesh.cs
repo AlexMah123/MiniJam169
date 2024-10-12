e0082b4856acac63373c3b57360d62794dc52de3
AlexMah123 <alexmahjy@gmail.com>
2024-10-12 20:00:01 +0800

    Level Design In Progress
    * Roads Places

--- Assets/Scripts/Mesh/CombineMesh.cs
+++ Assets/Scripts/Mesh/CombineMesh.cs
@@ -12,19 +12,29 @@ namespace Mesh
             MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
             CombineInstance[] combine = new CombineInstance[meshFilters.Length];
 
-            int i = 0;
-            while (i < meshFilters.Length)
+
+            for (int i = 0; i < meshFilters.Length; i++)
             {
+                if (meshFilters[i].gameObject == this.gameObject || meshFilters[i] == null)
+                {
+                    continue;      
+                }
+                
                 combine[i].mesh = meshFilters[i].sharedMesh;
                 combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
                 meshFilters[i].gameObject.SetActive(false);
-                
-                i++;
             }
-
-            transform.GetComponent<MeshFilter>().mesh = new UnityEngine.Mesh();
-            transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+  
+            
+            var meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = new UnityEngine.Mesh();
+            meshFilter.mesh.CombineMeshes(combine);
+            GetComponent<MeshCollider>().sharedMesh = meshFilter.mesh;
             transform.gameObject.SetActive(true);
+            
+            transform.localScale = Vector3.one;
+            transform.position = Vector3.zero;
+            transform.rotation = Quaternion.identity;
         }
     }
 }
